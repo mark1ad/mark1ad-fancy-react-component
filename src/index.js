b13df556077b93f21ff1a9f722eef01d9f916153
Mark De May <markd@frii.com>
2018-03-15 10:55:35 -0600

    Fanct verson 0.0.2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Fancy extends React.Component {
   render() {
     return (
-      <div>This is so Fancy!</div>
+      <div>This is so Fancy version 2!</div>
     );
   }
 }
